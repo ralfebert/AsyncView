afac9bfef117b49742d36ff88fc0a4fb61295402
Ralf Ebert <info@ralfebert.de>
2021-11-15 17:38:07 +0100

    rename to AsyncView

--- Package.swift
+++ Package.swift
@@ -1,29 +1,20 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "AsyncModel",
-    platforms: [
-        .iOS(.v15),
-    ],
+    name: "AsyncView",
+    platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AsyncModel",
-            targets: ["AsyncModel"]
+            name: "AsyncView",
+            targets: ["AsyncView"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "AsyncModel",
+            name: "AsyncView",
             dependencies: []
         ),
     ]
