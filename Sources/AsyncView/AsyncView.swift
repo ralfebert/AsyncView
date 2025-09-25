ded605747d80801aa114f1e4eb6400711f2dd240
Ralf Ebert <info@ralfebert.de>
2025-09-25 11:46:23 +0200

    Simplified AsyncView to use @State, removed model-based version

--- Sources/AsyncView/AsyncView.swift
+++ Sources/AsyncView/AsyncView.swift
@@ -1,16 +1,54 @@
 import SwiftUI
 
-public struct AsyncView<Success, Content: View>: View {
-    @StateObject var model: AsyncModel<Success>
-    let content: (_ item: Success) -> Content
+public struct AsyncView<Result, ResultView: View>: View {
+    let operation: () async throws -> Result
+    @ViewBuilder let content: (Result) -> ResultView
+    @State var state: AsyncState = .ready
+
+    public init(operation: @escaping () async throws -> Result, @ViewBuilder content: @escaping (Result) -> ResultView) {
+        self.operation = operation
+        self.content = content
+        self.state = self.state
+    }
+
+    enum AsyncState {
+        case ready
+        case progress
+        case loaded(Result)
+        case error(Error)
+    }
 
     public var body: some View {
-        AsyncModelView(model: self.model, content: self.content)
+        ZStack {
+            switch self.state {
+            case .ready:
+                EmptyView()
+            case .progress:
+                ProgressView()
+            case let .loaded(result):
+                self.content(result)
+            case let .error(error):
+                AsyncErrorView(
+                    error: error,
+                    reloadAction: {
+                        Task {
+                            await self.reload()
+                        }
+                    }
+                )
+            }
+        }
+        .task {
+            await self.reload()
+        }
     }
-}
 
-public extension AsyncView {
-    init(operation: @escaping AsyncModel<Success>.AsyncOperation, @ViewBuilder content: @escaping (_ item: Success) -> Content) {
-        self.init(model: AsyncModel(asyncOperation: operation), content: content)
+    func reload() async {
+        self.state = .progress
+        do {
+            self.state = try await .loaded(self.operation())
+        } catch {
+            self.state = .error(error)
+        }
     }
 }
